9271406ae07c9fdef24ae455813986cb28464f52
Haydart <makowiecki.radoslaw@gmail.com>
2017-09-06 23:58:56 +0200

    Add telephony manager to recognize incoming calls

--- app/src/main/java/background/PhoneStateChangedBroadcastReceiver.java
+++ app/src/main/java/background/PhoneStateChangedBroadcastReceiver.java
@@ -3,6 +3,7 @@ package background;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
@@ -11,6 +12,27 @@ import android.util.Log;
 public class PhoneStateChangedBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(getClass().getSimpleName(), "onReceive()");
+        Log.d(getClass().getSimpleName(), "phone state changed");
+
+        try {
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            PhoneStateListener PhoneListener = new PhoneStateListener();
+
+            telephonyManager.listen(PhoneListener, android.telephony.PhoneStateListener.LISTEN_CALL_STATE);
+        } catch (Exception ex) {
+            Log.e("Telephony manager error", ex.toString());
+        }
+    }
+
+    private class PhoneStateListener extends android.telephony.PhoneStateListener {
+
+        public void onCallStateChanged(int state, String incomingNumber) {
+            Log.d("MyPhoneListener", state + " incoming no:" + incomingNumber);
+
+            if (state == TelephonyManager.CALL_STATE_RINGING) {
+                String message = "New Phone Call Event. Incomming Number : " + incomingNumber;
+            }
+        }
     }
 }
